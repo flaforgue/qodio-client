66facb60238dbfb30f800c8e7e6d056fc66515ec
floLfg <florian.laforgue@neoxia.com>
2020-03-28 17:19:06 +0100

    Add dedicated canvas for Drones

--- src/services/canvas.service.ts
+++ src/services/canvas.service.ts
@@ -1,30 +1,67 @@
-import { Hive, PlayerType, Drone, Resource, Position } from '../types/qodio-front';
+import { Hive, PlayerType, Drone, Resource } from '../types/qodio-front';
 import colors from '../enums/colors';
-import { getColor } from '../utils';
+import { getColor, drawCircle } from '../utils';
 
 export default class CanvasService {
-  private _canvas: HTMLCanvasElement;
-  private _context: CanvasRenderingContext2D;
+  private _boardCanvas: HTMLCanvasElement;
+  private _boardContext: CanvasRenderingContext2D;
+
+  private _dronesCanvas: HTMLCanvasElement;
+  private _dronesContext: CanvasRenderingContext2D;
+
   private _hives: Hive[] = [];
   private _drones: Drone[] = [];
   private _resources: Resource[] = [];
   private _knownResources: Resource[] = [];
 
   public constructor() {
-    this._canvas = document.getElementById('board') as HTMLCanvasElement;
-    this._context = this._canvas.getContext('2d');
+    this._boardCanvas = document.getElementById('board') as HTMLCanvasElement;
+    this._boardContext = this._boardCanvas.getContext('2d');
+    this._dronesCanvas = document.getElementById('drones') as HTMLCanvasElement;
+    this._dronesContext = this._dronesCanvas.getContext('2d');
   }
 
   public set canvasHeight(height: number) {
-    this._canvas.height = height;
+    this._boardCanvas.height = height;
+    this._boardCanvas.style.height = `${height}px`;
+    this._dronesCanvas.height = height;
+    this._dronesCanvas.style.height = `${height}px`;
   }
 
   public set canvasWidth(width: number) {
-    this._canvas.width = width;
+    this._boardCanvas.width = width;
+    this._boardCanvas.style.width = `${width}px`;
+    this._dronesCanvas.width = width;
+    this._dronesCanvas.style.width = `${width}px`;
   }
 
   public redraw(): void {
-    this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    this._redrawBoardCanvas();
+    this._redrawDronesCanvas();
+  }
+
+  private _redrawDronesCanvas(): void {
+    this._dronesContext.clearRect(0, 0, this._dronesCanvas.width, this._dronesCanvas.height);
+
+    for (let i = 0; i < this._drones.length; i++) {
+      this._drawDrone(this._drones[i]);
+    }
+  }
+
+  public addDrones(playerType: PlayerType, drones: Drone[] = []): void {
+    this._drones.push(...drones.map((drone) => ({ ...drone, playerType })));
+  }
+
+  private _drawDrone(drone: Drone): void {
+    drawCircle(this._dronesContext, drone.position, 5, getColor(colors.actions[drone.action]));
+    if (drone.carriedResourceUnits > 0) {
+      drawCircle(this._dronesContext, drone.position, 3, getColor(colors.knownResource));
+    }
+  }
+
+  private _redrawBoardCanvas(): void {
+    this._boardContext.clearRect(0, 0, this._boardCanvas.width, this._boardCanvas.height);
+
     for (let i = 0; i < this._hives.length; i++) {
       this._drawHive(this._hives[i]);
     }
@@ -35,10 +72,6 @@ export default class CanvasService {
 
     for (let i = 0; i < this._knownResources.length; i++) {
       this._drawKnownResource(this._knownResources[i]);
-    }
-
-    for (let i = 0; i < this._drones.length; i++) {
-      this._drawDrone(this._drones[i]);
     }
   }
 
@@ -53,10 +86,6 @@ export default class CanvasService {
     this._hives.push({ ...hive, playerType });
   }
 
-  public addDrones(playerType: PlayerType, drones: Drone[] = []): void {
-    this._drones.push(...drones.map((drone) => ({ ...drone, playerType })));
-  }
-
   public addKnownResources(resources: Resource[] = []): void {
     this._knownResources.push(...resources);
   }
@@ -66,34 +95,36 @@ export default class CanvasService {
   }
 
   private _drawHive(hive: Hive): void {
-    this._drawCircle(
+    drawCircle(
+      this._boardContext,
       hive.position,
       hive.territoryRadius,
       getColor(colors.players[hive.playerType], 0.2),
     );
 
-    this._drawCircle(hive.position, hive.radius, getColor(colors.players[hive.playerType]));
-  }
-
-  private _drawDrone(drone: Drone): void {
-    this._drawCircle(drone.position, 5, getColor(colors.actions[drone.action]));
-    if (drone.carriedResourceUnits > 0) {
-      this._drawCircle(drone.position, 3, getColor(colors.knownResource));
-    }
+    drawCircle(
+      this._boardContext,
+      hive.position,
+      hive.radius,
+      getColor(colors.players[hive.playerType]),
+    );
   }
 
   private _drawResource(resource: Resource): void {
-    this._drawCircle(resource.position, resource.stock / 5, getColor(colors.resource));
+    drawCircle(
+      this._boardContext,
+      resource.position,
+      resource.stock / 5,
+      getColor(colors.resource),
+    );
   }
 
   private _drawKnownResource(resource: Resource): void {
-    this._drawCircle(resource.position, resource.stock / 5, getColor(colors.knownResource));
-  }
-
-  private _drawCircle(position: Position, radius: number, color: string): void {
-    this._context.beginPath();
-    this._context.arc(position.x, position.y, radius, 0, 2 * Math.PI);
-    this._context.fillStyle = color;
-    this._context.fill();
+    drawCircle(
+      this._boardContext,
+      resource.position,
+      resource.stock / 5,
+      getColor(colors.knownResource),
+    );
   }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,19 @@
+import { Position } from '../types/qodio-front';
+
 const getColor = (data: number[], opacity = 1): string => {
   return `rgba(${data.join(',')}, ${opacity})`;
 };
 
-export { getColor };
+const drawCircle = (
+  context: CanvasRenderingContext2D,
+  position: Position,
+  radius: number,
+  color: string,
+): void => {
+  context.beginPath();
+  context.arc(position.x, position.y, radius, 0, 2 * Math.PI);
+  context.fillStyle = color;
+  context.fill();
+};
+
+export { getColor, drawCircle };
